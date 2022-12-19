185d3cdef6c4a6aaa15d7c586e4e76d2cee4110b
Y_Less <git-athena@y-less.com>
2022-12-19 01:14:49 +0000

    Fix https://github.com/compuphase/pawn/issues/36

--- NativesMain.hpp
+++ NativesMain.hpp
@@ -15,26 +15,15 @@ int AmxLoad(AMX* amx)
         ret
         = 0;
 #ifdef PAWN_NATIVES_HAS_FUNC
-    if (NativeFuncBase::all_) {
-        // Need a sentinel because of:
-        //
-        //   https://github.com/Zeex/sampgdk/issues/188
-        //
-        // Otherwise we could do:
-        //
-        //   curNative.name = curFunc->name_;
-        //   curNative.func = curFunc->native_;
-        //   ret = amx_Register(amx, &curNative, 1);
-        //
-        AMX_NATIVE_INFO
-        curNative[2] = { { 0, 0 }, { 0, 0 } };
-        for (NativeFuncBase* curFunc : *NativeFuncBase::all_) {
-            LOG_NATIVE_INFO("Registering native %s", curFunc->name_);
-            curNative[0].name = curFunc->name_;
-            curNative[0].func = curFunc->native_;
-            ret = amx_Register(amx, curNative, -1);
-        }
-    }
+	if (NativeFuncBase::all_) {
+		AMX_NATIVE_INFO curNative;
+		for (NativeFuncBase* curFunc : *NativeFuncBase::all_) {
+			LOG_NATIVE_INFO("Registering native %s", curFunc->name_);
+			curNative.name = curFunc->name_;
+			curNative.func = curFunc->native_;
+			ret = amx_Register(amx, &curNative, 1);
+		}
+	}
 #endif
     return ret;
 }
